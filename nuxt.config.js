fbe5eb43a068d093b5b41431286c1ef190a505e8
viandwi24 <fiandwi0424@gmail.com>
2021-01-09 20:08:05 -0800

    -

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+import path from 'path'
+
+export default {
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'viraa-dashboard',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    '~/assets/scss/vendor.scss',
+    '~/assets/scss/app.scss'
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    '@nuxtjs/google-fonts'
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    'nuxt-purgecss'
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+    extractCSS: false,
+    postcss: {
+      plugins: { tailwindcss: path.resolve(__dirname, './tailwind.config.js') }
+    }
+  },
+
+  // purge css
+  purgeCSS: { mode: 'postcss' },
+
+  // fonts
+  googleFonts: {
+    families: {
+      Raleway: true
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,236 @@
+<template>
+  <div class="dashboard">
+    <div class="sidebar left">
+      <div class="brand">
+        <a href="#">
+          <img src="~/assets/images/logo.svg" class="inline-block mr-2" width="34px">
+          Viraa
+        </a>
+      </div>
+      <div class="scrollable">
+        <div class="menu">
+          <div class="item active">
+            <a href="#">
+              <home-icon size="1.2x" class="icon" />
+              <span class="text">Dashboard</span>
+            </a>
+          </div>
+          <div class="item">
+            <a href="#">
+              <edit-icon size="1.2x" class="icon" />
+              <span class="text">Posts</span>
+            </a>
+          </div>
+          <div class="item">
+            <a href="#">
+              <folder-icon size="1.2x" class="icon" />
+              <span class="text">Storage</span>
+            </a>
+          </div>
+          <div class="item">
+            <a href="#">
+              <message-square-icon size="1.2x" class="icon" />
+              <span class="text">Chat</span>
+            </a>
+          </div>
+          <div class="item">
+            <a href="#">
+              <calendar-icon size="1.2x" class="icon" />
+              <span class="text">Planning</span>
+            </a>
+          </div>
+          <div class="item">
+            <a href="#">
+              <settings-icon size="1.2x" class="icon" />
+              <span class="text">Setting</span>
+            </a>
+          </div>
+        </div>
+        <div class="footer px-6 pb-6">
+          <div class="card bg-blue-100 p-4 rounded-xl">
+            <div class="text-center mb-2" style="margin-top: -35px;">
+              <img src="~/assets/images/icons/upgrade-premium.svg" alt="Upgrade!" class="inline-block">
+            </div>
+            <p class="text-sm text-center mb-4">
+              Upgrade to <b>PRO</b> for more resources.
+            </p>
+            <a href="#" class="bg-blue-700 inline-block w-full py-2 rounded-xl text-gray-100 font-semibold text-center">
+              Uppgrade
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="content container mx-auto px-8">
+      <!-- section header -->
+      <div class="header">
+        <h1>Dashboard</h1>
+        <div class="right">
+          <span class="text-sm text-gray-500">Saturday, 9 Jan 2021</span>
+          <button class="bg-blue-200 px-2 py-2 rounded-lg text-blue-700 font-semibold inline ml-2">
+            <search-icon size="1.2x" />
+          </button>
+        </div>
+      </div>
+
+      <!-- section welcome card -->
+      <div class="card mt-12 bg-red-100 block rounded-xl py-8 px-10">
+        <div class="flex">
+          <div class="text w-6/12">
+            <span class="font-bold text-xl text-pink-600">Welcome back Alfian!</span>
+            <p class="pt-4 text-sm text-gray-800">
+              You've learned 80% of your goal this week!<br>
+              Keep it up and improve your results!
+            </p>
+          </div>
+          <div class="illustration w-6/12">
+            <img src="~/assets/images/illustrations/1.svg" class="w-full" style="margin-top: -80px;">
+          </div>
+        </div>
+      </div>
+
+      <!-- section post -->
+      <div class="mt-6">
+        <div class="font-semibold mb-4">
+          Your posts
+        </div>
+        <div class="list gap-3">
+          <div class="item rounded-xl w-full lg:w-1/3 flex bg-red-400">
+            <div class="main p-4 self-center">
+              <div class="px-4 py-2 rounded-xl bg-red-600 text-xl text-gray-100">
+                10
+              </div>
+            </div>
+            <div class="text self-center w-full">
+              <div class="text-sm text-gray-50">
+                Published
+              </div>
+              <div class="font-semibold text-gray-50 pr-4">
+                <a href="#" class="flex justify-between">
+                  <span>Total Comments</span>
+                  <arrow-right-icon size="1.2x" class="self-center" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="item rounded-xl w-full lg:w-1/3 flex bg-purple-400">
+            <div class="main p-4 self-center">
+              <div class="px-4 py-2 rounded-xl bg-purple-600 text-xl text-gray-100">
+                4
+              </div>
+            </div>
+            <div class="text self-center w-full">
+              <div class="text-sm text-gray-50">
+                Published
+              </div>
+              <div class="font-semibold text-gray-50 pr-4">
+                <a href="#" class="flex justify-between">
+                  <span>Total Comments</span>
+                  <arrow-right-icon size="1.2x" class="self-center" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="item rounded-xl w-full lg:w-1/3 flex bg-blue-400">
+            <div class="main p-4 self-center">
+              <div class="px-4 py-2 rounded-xl bg-blue-600 text-xl text-gray-100">
+                1
+              </div>
+            </div>
+            <div class="text self-center w-full">
+              <div class="text-sm text-gray-50">
+                Drafted
+              </div>
+              <div class="font-semibold text-gray-50 pr-4">
+                <a href="#" class="flex justify-between">
+                  <span>Total Draft</span>
+                  <arrow-right-icon size="1.2x" class="self-center" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sidebar right">
+      <div class="container mx-auto px-8 pt-6">
+        <div class="menu-logout">
+          <a href="#" class="flex justify-between text-gray-800">
+            <span class="font-semibold">Logout</span>
+            <log-out-icon size="1.2x" />
+          </a>
+        </div>
+        <div class="profile mt-8">
+          <img src="~/assets/images/profile.svg" alt="Avatar" class="avatar" width="140px">
+          <div class="font-semibold mt-4 text-gray-800">
+            Alfian Dwi Nugraha
+          </div>
+          <div class="text-xs mt-1 text-gray-600">
+            viandwicyber@gmail.com
+          </div>
+        </div>
+        <div class="mt-10">
+          <div class="flex justify-between">
+            <span class="font-semibold">Notifications</span>
+            <bell-icon size="1.2x" class="text-gray-500" />
+          </div>
+          <div class="list mt-3">
+            <div class="item my-2 flex gap-2">
+              <div class="icon w-2/12 bg-red-100 text-red-500 rounded-lg">
+                <alert-circle-icon size="1.2x" />
+              </div>
+              <div class="content w-10/12">
+                <div class="font-semibold text-gray-800">
+                  Your plan started in free.
+                </div>
+                <div class="text-xs text-gray-500 mt-1">
+                  Friday, 8 Jan 2021
+                </div>
+              </div>
+            </div>
+            <div class="item my-2 flex gap-2">
+              <div class="icon w-2/12 bg-blue-100 text-blue-500 rounded-lg">
+                <mail-icon size="1.2x" />
+              </div>
+              <div class="content w-10/12">
+                <div class="font-semibold text-gray-800">
+                  Receive new message.
+                </div>
+                <div class="text-xs text-gray-500 mt-1">
+                  Saturday, 9 Jan 2021
+                </div>
+              </div>
+            </div>
+            <div class="item my-2 flex gap-2">
+              <div class="icon w-2/12 bg-blue-100 text-blue-500 rounded-lg">
+                <eye-icon size="1.2x" />
+              </div>
+              <div class="content w-10/12">
+                <div class="font-semibold text-gray-800">
+                  Newest post got 100 view.
+                </div>
+                <div class="text-xs text-gray-500 mt-1">
+                  Saturday, 9 Jan 2021
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+const { HomeIcon, EditIcon, SettingsIcon, FolderIcon, MessageSquareIcon, CalendarIcon, SearchIcon, LogOutIcon, BellIcon, AlertCircleIcon, MailIcon, EyeIcon, ArrowRightIcon } = require('vue-feather-icons')
+
+export default Vue.extend({
+  components: {
+    HomeIcon, EditIcon, SettingsIcon, FolderIcon, MessageSquareIcon, CalendarIcon, SearchIcon, LogOutIcon, BellIcon, AlertCircleIcon, MailIcon, EyeIcon, ArrowRightIcon
+  }
+})
+</script>
+
+<style lang="scss">
+</style>
